bfda2564673f246c75ee862cafa0d896e3442f0a
upgradingdave <upgradingdave@gmail.com>
2016-01-29 16:23:54 -0500

    Euler Problem 3 in haskell

--- hs/hs-practice.hs
+++ hs/hs-practice.hs
@@ -5,17 +5,35 @@
 problem1 :: (Integral a) => a -> a
 problem1 x = sum [y | y <- [1..(x - 1)], (y `mod` 3 == 0) || (y `mod` 5 == 0)]
 
-
-nextfib :: Num a => a -> a -> [a]
-nextfib x y = x + y : nextfib y (x + y)
-
+fib :: [Integer]
 fib = nextfib 0 1
+    where nextfib x y = x + y : nextfib y (x + y)
 
 -- Problem 2
 -- Find the sum of even valued fibbonacci numbers less than n
---problem2 :: (Integer a) => a -> a
+problem2 :: Integer -> Integer
 problem2 n = sum (takeWhile (\x -> x < n) (filter even (fib)))
 -- Test: problem2 4000000 == 4613732
+
+prime :: Integer -> Bool
+prime n = prime' n (n-1)
+    where prime' x 1 = True
+          prime' x y = if x `mod` y == 0 then False else prime' x (y-1)
+
+-- Problem 3
+-- Find the largest prime factor of the number n
+-- 1) start with i=2
+-- 2a) is i>=n? if so, it's the largest prime factor
+-- 2b) is does i divide n evenly?
+-- 3)  if yes, then set n = n/i and go to '2'
+-- 4)  if no, increment i and goto '2'
+problem3 :: Integral a => a -> a
+problem3 n = problem3' n 2
+    where problem3' n i
+              | i >= n = i
+              | n `mod` i == 0 = problem3' (n `div` i) i
+              | otherwise = problem3' n (i+1)
+-- Test: problem3 600851475143 == 6857
 
 -- Problem 9
 -- A Pythagorean triplet is a set of three natural numbers, a < b < c,
