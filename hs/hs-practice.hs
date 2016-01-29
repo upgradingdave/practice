74446cc7e00f3710bfa0605a43c8bfa508cc467e
upgradingdave <upgradingdave@gmail.com>
2016-01-29 15:37:59 -0500

    Euler Problem 2 in haskell

--- hs/hs-practice.hs
+++ hs/hs-practice.hs
@@ -5,11 +5,23 @@
 problem1 :: (Integral a) => a -> a
 problem1 x = sum [y | y <- [1..(x - 1)], (y `mod` 3 == 0) || (y `mod` 5 == 0)]
 
+
+nextfib :: Num a => a -> a -> [a]
+nextfib x y = x + y : nextfib y (x + y)
+
+fib = nextfib 0 1
+
+-- Problem 2
+-- Find the sum of even valued fibbonacci numbers less than n
+--problem2 :: (Integer a) => a -> a
+problem2 n = sum (takeWhile (\x -> x < n) (filter even (fib)))
+-- Test: problem2 4000000 == 4613732
+
 -- Problem 9
 -- A Pythagorean triplet is a set of three natural numbers, a < b < c,
 -- for which, a^2 + b^2 = c^2. For example, 32 + 42 = 9 + 16 = 25 = 52.
 -- There exists exactly one Pythagorean triplet for which a + b + c =
 -- 1000. Find the product abc.
--- TODO: In progress!
-problem9 :: (Integral a) => a -> [(a,a,a)]
-problem9 x = [(a,b,c) | a+b+c == x, a < b, b < c]
+-- TODO
+-- problem9 :: (Integral a) => a -> [(a,a,a)]
+-- problem9 x = [(a,b,c) | a+b+c == x, a < b, b < c]
