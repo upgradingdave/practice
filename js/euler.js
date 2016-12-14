ce83fc1bea3a6e16bdbfc2680b71398fb1ca932e
upgradingdave <upgradingdave@gmail.com>
2016-12-13 21:12:46 -0500

    project euler problem 23 and 24

--- js/euler.js
+++ js/euler.js
@@ -75,7 +75,7 @@ module.exports = {
    * 91 × 99.
    *
    * Find the largest palindrome made from the product of two 3-digit
-   *  numbers. 
+   *  numbers.
    */
   problem4: function(n) {
     var s = "";
@@ -114,6 +114,229 @@ module.exports = {
       }
       if(divides) return i;
     }
-  }
+  },
 
+  factors: function(n) {
+    var results = new Set([1]);
+    if(n==1) return Array.from(results);
+    for(var i=2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        results.add(i);
+        results.add(n/i);
+      }
+    }
+    return Array.from(results);
+  },
+
+  /*
+   * Returns 0 if perfect, + if abundant, - if deficient
+   */
+  isPerfect: function(n) {
+    var fs = this.factors(n);
+    var sum = 0;
+    for(var j=0, len=fs.length; j<len; j++){
+      sum += fs[j];
+    }
+    return sum - n;
+  },
+
+  /*
+   * Find abundant numbers less than x
+   */
+  abundantNumbers: function(x) {
+    let abundant = [];
+    for(var i=2; i<x; i++) {
+      if(this.isPerfect(i) > 0) {
+        abundant.push(i);
+      }
+    }
+    return abundant;
+  },
+
+  /*
+   * Find all valid sums of abundant numbers less than x
+   */
+  abundantSums: function (m) {
+    // initialize array of size m to all false
+    let result = [];
+    for(let x=0; x<m; x++) {
+      result[x] = false;
+    }
+
+    // combine all possible sums of 2 abundantNums below m. Mark
+    // position of array true.
+    let abundantNums = this.abundantNumbers(m);
+    for(let x=0;x<abundantNums.length;x++) {
+      for(let y=x;y<abundantNums.length;y++) {
+        let sum = abundantNums[x] + abundantNums[y];
+        if(sum > m) {
+          continue;
+        } else {
+          result[sum] = true;
+        }
+      }
+    }
+    return result;
+  },
+
+  problem23: function() {
+    // find all integers which cannot be written as the sum of two abundant
+    // numbers
+    let abundantSum = this.abundantSums(28124);
+    let sum = 0;
+    for(let i=0; i<28214; i++) {
+      if(abundantSum[i]==false) {
+        sum = sum + i;
+      }
+    }
+    return sum;
+  },
+
+  shift: function(arr) {
+    if(arr.length <= 1) {
+      return arr;
+    }
+
+    let newArray = [];
+    for(let i=1; i<arr.length; i++) {
+      newArray.push(arr[i]);
+    }
+    newArray.push(arr[0]);
+    return newArray;
+  },
+
+  swap: function(s, i, j) {
+    result = [];
+    for(let x=0; x<s.length; x++) {
+      if(x==i) {
+        result = result + s[j];
+      } else if(x ==j) {
+        result = result + s[i];
+      } else {
+        result = result + s[x];
+      }
+    }
+    return result;
+  },
+
+  factorial: function(n) {
+    let result = 1;
+    for(let i=n; i>0; i--) {
+      result = result * i;
+    }
+    return result;
+  },
+
+  reverse: function(s) {
+    let i=0,j=s.length-1;
+    while(j>i) {
+      s = this.swap(s,i,j);
+      i++;
+      j--;
+    }
+    return s;
+  },
+
+  logEnabled : false,
+  log : function() {
+    if(this.logEnabled && console) {
+      console.log.apply(console, arguments);
+    }
+  },
+
+  /*
+   * What is the millionth lexicographic permutation of the digits 0, 1,
+   * 2, 3, 4, 5, 6, 7, 8 and 9?
+   * Find the next lexicographic permutation
+   * https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+   */
+  nextLexicographicalPermutation: function(s) {
+    //this.logEnabled = true;
+    this.log("compute next permutation");
+
+    let len = s.length;
+    let c = s.slice();
+
+    // find the "weakly decreasing suffix". In other words scan from right to
+    // left until you find a number greater than the previous number. This
+    // is the index where the suffix starts
+    let i=len-1;
+    while(i>0 && c[i] <= c[i-1]) {
+      i--;
+    }
+
+    this.log("i:"+i);
+
+    // Set the "pivot" index to the be the index just to the left of i. If we're
+    // already at the beginning index, then there are no more permutations
+    let p = 0;
+    if(i<=0){
+      return null;
+    } else {
+      p = i-1;
+    }
+
+    this.log("p:"+p);
+
+    // find the smallest right most number larger than the pivot. In other
+    // words, find the smallest number in the suffix bigger than the pivot.
+    for(let x=i;x<len;x++){
+      if(c[x] > c[p]) {
+        j = x;
+      }
+    }
+
+    this.log("j:"+j);
+
+    // swap j and p
+    c = this.swap(c, j, p);
+
+    this.log("after swap:"+c);
+
+    // reverse the suffix
+    let h=i,g=len-1;
+    while(h<g) {
+      c = this.swap(c,h,g);
+      h++;
+      g--;
+    }
+
+    this.log("after reverse: "+ c);
+
+    return c;
+  },
+
+  /*
+   * Use factorials to figure out permutations to start from
+   */
+  calcPermStart: function() {
+    let s = "0123456789";
+    let total = 1000000;
+    let result = [];
+
+    for(let i=9; i>0 && total > 0; i--) {
+      let f = this.factorial(i);
+      let n = Math.floor(total / f);
+      result.push(n);
+      total = total - (n * f);
+      this.log(total);
+    }
+  },
+
+  bruteNthPerm: function(start, nth) {
+
+    for(let i=0; i<nth; i++) {
+      this.log(i+"'> " + start);
+      start = this.nextLexicographicalPermutation(start);
+      //console.log(i+"> " + start);
+    }
+    return start;
+  },
+
+  problem24: function() {
+    //2783915604
+    return this.bruteNthPerm("0123456789", 999999);
+
+    // TODO: non brute force method
+  }
 };
--- js/test.js
+++ js/test.js
@@ -43,3 +43,65 @@ describe('euler problem 5', function() {
     assert.equal(232792560, euler.problem5(20));
   });
 });
+
+describe('euler problem23', function() {
+  it('should find non abundant numbers', function () {
+    assert.deepEqual([1], euler.factors(1));
+    assert.deepEqual([1, 2], euler.factors(4));
+    assert.deepEqual([1,2,14,4,7], euler.factors(28));
+    assert.equal(true, euler.isPerfect(4) < 0);
+    assert.equal(true, euler.isPerfect(28) == 0);
+    assert.equal(true, euler.isPerfect(12) > 0);
+    assert.equal(4179871, euler.problem23());
+  });
+});
+
+describe('factorial', function() {
+  it('should find factorial', function () {
+    assert.equal(1, euler.factorial(1));
+    assert.equal(2, euler.factorial(2));
+    assert.equal(6, euler.factorial(3));
+  });
+});
+
+describe('swap', function() {
+  it('should swap chars in strings', function () {
+    assert.equal("1023", euler.swap("0123",0,1));
+    assert.equal("3120", euler.swap("0123",0,3));
+  });
+});
+
+describe('reverse', function() {
+  it('should reverse string', function () {
+    assert.equal("1023", euler.reverse("3201"));
+    assert.equal("102", euler.reverse("201"));
+    assert.equal("123456789012345678901234567890",
+      euler.reverse("098765432109876543210987654321"));
+  });
+});
+
+describe('euler problem24', function() {
+  it('should permute', function () {
+    assert.deepEqual([1,0], euler.shift([0,1]));
+    assert.deepEqual([1,2,0], euler.shift([0,1,2]));
+
+    assert.equal("10", euler.nextLexicographicalPermutation("01"));
+    assert.equal("021", euler.nextLexicographicalPermutation("012"));
+    assert.equal("0123456798",
+      euler.nextLexicographicalPermutation("0123456789"));
+    assert.equal("2701345869",
+      euler.nextLexicographicalPermutation("2701345698"));
+
+    assert.equal("0123456978",
+      euler.nextLexicographicalPermutation("0123456897"));
+
+    // assert.equal("2701345869",
+    //   euler.nextLexicographicalPermutation("2701345869"));
+    assert.equal(362880, euler.factorial(9));
+    assert.equal(40320, euler.factorial(8));
+
+    assert.equal("0123457968", euler.bruteNthPerm("0123456789", 10));
+    assert.equal("210", euler.bruteNthPerm("012", 5));
+    //assert.equal("2783915460", euler.problem24());
+  });
+});
